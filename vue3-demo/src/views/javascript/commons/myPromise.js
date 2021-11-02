374c621cc469f89039facecb5a012e6e01c33577
lhd <1625125333@qq.com>
2021-11-02 08:34:48 +0800

    给 promise 添加 all race catch reject resovle 等方法

--- vue3-demo/src/views/javascript/commons/myPromise.js
+++ vue3-demo/src/views/javascript/commons/myPromise.js
@@ -58,50 +58,19 @@ function MyPromise(executor) {
 //因为 then 方法是公共的，所以定义在 MyPromise 的原型中，不用每次 new 的时候都去创建该方法
 MyPromise.prototype.then = function (onResolved, onRejected) {
     return new MyPromise((resolve, reject) => {
+        if (!isFunction(onResolved)) onResolved = value => value; // 把值传递下去
+        if (!isFunction(onRejected)) onRejected = value => value;
         switch (this.status) {
             case STATUS.pending:
                 // 如果没有传入回调函数或者不是函数就忽略
                 // 否则就放入数组中等状态改变时用来执行相应的方法
                 // isFunction(onResolved) && this.onResolvedArray.push(onResolved);
                 // isFunction(onRejected) && this.onRejectedArray.push(onRejected);
-                if (isFunction(onResolved)) {
-                    this.onResolvedArray.push(() => {
-                        setTimeout(() => {
-                            try {
-                                const res = onResolved(this.value);
-                                if (res instanceof MyPromise) {
-                                    res.then(res => {
-                                        resolve(res);
-                                    });
-                                } else {
-                                    resolve(res);
-                                }
-                            } catch (error) {
-                                reject(error);
-                            }
-                        }, 0);
-                    });
-                }
-                if (isFunction(onRejected)) {
-                    this.onRejectedArray.push(() => {
-                        setTimeout(() => {
-                            try {
-                                const err = onRejected(this.value);
-                                resolve(err);
-                            } catch (error) {
-                                reject(error);
-                            }
-                        }, 0);
-                    });
-                }
-                break;
-            case STATUS.resolved:
-                // isFunction(onResolved) && (this.value = onResolved(this.value));
-                if (isFunction(onResolved)) {
+                // if (isFunction(onResolved)) {
+                this.onResolvedArray.push(() => {
                     setTimeout(() => {
                         try {
                             const res = onResolved(this.value);
-                            // 将上次一then里面的方法传递进下一个 Promise 的状态
                             if (res instanceof MyPromise) {
                                 res.then(res => {
                                     resolve(res);
@@ -113,11 +82,10 @@ MyPromise.prototype.then = function (onResolved, onRejected) {
                             reject(error);
                         }
                     }, 0);
-                }
-                break;
-            case STATUS.rejected:
-                isFunction(onRejected) && (this.reason = onRejected(this.reason));
-                if (isFunction(onRejected)) {
+                });
+                // }
+                // if (isFunction(onRejected)) {
+                this.onRejectedArray.push(() => {
                     setTimeout(() => {
                         try {
                             const err = onRejected(this.reason);
@@ -126,7 +94,41 @@ MyPromise.prototype.then = function (onResolved, onRejected) {
                             reject(error);
                         }
                     }, 0);
-                }
+                });
+                // }
+                break;
+            case STATUS.resolved:
+                // isFunction(onResolved) && (this.value = onResolved(this.value));
+                // if (isFunction(onResolved)) {
+                setTimeout(() => {
+                    try {
+                        const res = onResolved(this.value);
+                        // 将上次一then里面的方法传递进下一个 Promise 的状态
+                        if (res instanceof MyPromise) {
+                            res.then(res => {
+                                resolve(res);
+                            });
+                        } else {
+                            resolve(res);
+                        }
+                    } catch (error) {
+                        reject(error);
+                    }
+                }, 0);
+                // }
+                break;
+            case STATUS.rejected:
+                // isFunction(onRejected) && (this.reason = onRejected(this.reason));
+                // if (isFunction(onRejected)) {
+                setTimeout(() => {
+                    try {
+                        const err = onRejected(this.reason);
+                        reject(err);
+                    } catch (error) {
+                        reject(error);
+                    }
+                }, 0);
+                // }
                 break;
             default:
                 break;
@@ -134,11 +136,76 @@ MyPromise.prototype.then = function (onResolved, onRejected) {
     });
 };
 
+MyPromise.prototype.catch = function (reject) {
+    return this.then(undefined, reject);
+};
+
+MyPromise.prototype.resolve = function (value) {
+    if (value instanceof MyPromise) return value;
+    return new MyPromise(resovle => resovle(value));
+};
+
+MyPromise.prototype.reject = function (reason) {
+    return new MyPromise((resovle, reject) => reject(reason));
+};
+
+MyPromise.prototype.all = function (promiseArray) {
+    if (promiseArray instanceof Array) throw new Error('参数为非数组');
+    let i = 0;
+    const result = [];
+    return new MyPromise((resovle, reject) => {
+        promiseArray.forEach((promise, index) => {
+            // Promise.resolve(p) 用于处理传入值不为 Promise 的情况
+            MyPromise.resovle(promise).then(
+                res => {
+                    i++;
+                    result[index] = res;
+                    //所有then执行后, resolve 结果，返回结果集
+                    if (i === promiseArray.length) {
+                        resovle(result);
+                    }
+                },
+                err => {
+                    //有一个Promise被reject时，MyPromise的状态变为rejec
+                    reject(err);
+                },
+            );
+        });
+    });
+};
+
+MyPromise.prototype.race = function (promiseArray) {
+    if (promiseArray instanceof Array) throw new Error('参数为非数组');
+    return new MyPromise((resovle, reject) => {
+        promiseArray.forEach(promise => {
+            // Promise.resolve(p) 用于处理传入值不为 Promise 的情况
+            MyPromise.resovle(promise).then(
+                res => {
+                    resovle(res);
+                },
+                err => {
+                    reject(err);
+                },
+            );
+        });
+    });
+};
+
+// Promise.prototype.finally = function (callback) {
+//     return this.then(
+//         value => MyPromise.resolve(callback()).then(() => value),
+//         reason =>
+//             MyPromise.resolve(callback()).then(() => {
+//                 throw reason;
+//             }),
+//     );
+// };
+
 const myPromise = new MyPromise((resolve, reject) => {
     // resolve(123);
     setTimeout(() => {
-        resolve(123);
-        // reject(123);
+        // resolve(123);
+        reject(123);
     }, 1000);
 });
 // const myPromise = new Promise((resolve, reject) => {
@@ -161,9 +228,18 @@ const mp1 = myPromise
             return 1;
         },
     )
-    .then(res => {
-        console.log('myPromise.then resolve--------------------------2>>>', res);
-        return 2;
+    .then(
+        res => {
+            console.log('myPromise.then resolve--------------------------2>>>', res);
+            return 2;
+        },
+        // res => {
+        //     console.log('myPromise.then reject---------------------------2>>>', res);
+        //     return 2;
+        // },
+    )
+    .catch(err => {
+        console.log('myPromise.then resolve--------------------------catch>>>', err);
     });
 
 const mp2 = mp1
@@ -196,9 +272,9 @@ mp2.then(
         console.log('myPromise.then resolve--------------------------6>>>', res);
         return 7;
     },
-    res => {
-        console.log('myPromise.then reject---------------------------6>>>', res);
-    },
+    // res => {
+    //     console.log('myPromise.then reject---------------------------6>>>', res);
+    // },
 );
 
 mp3.then(
@@ -206,7 +282,7 @@ mp3.then(
         console.log('myPromise.then resolve--------------------------8>>>', res);
         return 8;
     },
-    res => {
-        console.log('myPromise.then reject---------------------------8>>>', res);
-    },
+    // res => {
+    //     console.log('myPromise.then reject---------------------------8>>>', res);
+    // },
 );
