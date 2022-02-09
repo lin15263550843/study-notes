3a83d573a08ece8637a48e4c2bbab14592179144
lhd <1625125333@qq.com>
2022-02-09 14:25:44 +0800

    删除无效的括号

--- vite-vue-demo/src/interviewQuestions/eligibleParentheses.ts
+++ vite-vue-demo/src/interviewQuestions/eligibleParentheses.ts
@@ -1,78 +1,106 @@
 // @ts-nocheck
 /**
+ * 删除无效的括号
  * 给一个字符窜括号：  ））（（）））（）（）（（  输出合格的括号： （（））（）（）
  */
-function test(str) {
-    if (typeof str !== 'string') return '';
+// function test(str) {
+//     if (typeof str !== 'string') return '';
 
-    const a = str.split('');
-    const res = [];
-    const cache = [];
-    const aLen = a.length;
+//     const a = str.split('');
+//     // const aLen = a.length;
+//     const cache = [];
+//     let res = [];
+//     let len = cache.length;
 
-    for (let i = 0; i < aLen - 1; i++) {
-        const cur = a[i];
-        const last = res[res.length - 1];
+//     // for (let i = 0; i < aLen - 1; i++) {
+//     //     const cur = a[i];
 
-        if (cur === '（') {
-            res.push(cur);
-        } else {
-            const cLen = cache.length;
-            if (cLen > 0) {
-                console.log('res[res.length - 1 - cLen]', cLen, res[res.length - 1 - cLen]);
-                if (res[res.length - 1 - cLen] === '（') {
-                    cache.push(cur);
-                } else {
-                    while (cache.length > 0) {
-                        res.push(cache.pop());
-                    }
+//     //     if (cur === '（') {
+//     //         cache.push(cur);
+//     //     } else {
+//     //         if (len > 0) {
+//     //             res = [cache.pop(), ...res, cur];
+//     //         } else {
+//     //             if (cache.length > 0) {
+//     //                 res.push(cache.pop());
+//     //                 res.push(cur);
+//     //             }
+//     //         }
+//     //         len = cache.length;
+//     //     }
+//     // }
+//     console.log('-----------------------------------');
+//     console.log('cache', cache);
+//     console.log('res', res);
+
+//     a.forEach(cur => {
+//         if (cur === '（') {
+//             cache.push(cur);
+//         } else {
+//             if (len > 0) {
+//                 res = [cache.pop(), ...res, cur];
+//             } else {
+//                 if (cache.length > 0) {
+//                     res.push(cache.pop());
+//                     res.push(cur);
+//                 }
+//             }
+//             len = cache.length;
+//         }
+//     });
+//     return res.join('');
+// }
+// console.log('test --->>>', test('））（（）））（）（）（（'));
+// console.log('test --->>>', test('））（（（）））（）（（））（（'));
+var removeInvalidParentheses = function (s) {
+    const ans = [];
+    let currSet = new Set();
+
+    currSet.add(s);
+    while (true) {
+        for (const str of currSet) {
+            if (isValid(str)) {
+                ans.push(str);
+            }
+        }
+        if (ans.length > 0) {
+            return ans;
+        }
+        const nextSet = new Set();
+        for (const str of currSet) {
+            for (let i = 0; i < str.length; i++) {
+                if (i > 0 && str[i] === str[i - 1]) {
+                    continue;
                 }
-            } else {
-                if (last === '（') {
-                    cache.push(cur);
+                if (str[i] === '(' || str[i] === ')') {
+                    nextSet.add(str.substring(0, i) + str.substring(i + 1));
                 }
             }
         }
-
-        // if (cLen > 0) {
-        //     if(res[c])
-        // }
+        currSet = nextSet;
     }
-    console.log(cache);
-    console.log(res);
+};
 
-    // a.forEach(cur => {
-    //     if (cur === '（') {
-    //         res.push(cur);
-    //     } else {
-    //         const last = res[res.length - 1];
+const isValid = str => {
+    let count = 0;
 
-    //         if (last === '（') {
-    //             let len = arr.length;
+    for (const c of str) {
+        if (c === '(') {
+            count++;
+        } else if (c === ')') {
+            count--;
+            if (count < 0) {
+                return false;
+            }
+        }
+    }
 
-    //             if (len === 0) {
-    //                 arr.push(cur);
-    //                 arr.push(res.pop());
-    //             } else {
-    //                 if (arr.length > 0) {
-    //                     while (arr.length > 0) {
-    //                         res.push(arr.pop());
-    //                     }
-    //                 }
+    return count === 0;
+};
 
-    //                 arr.push(cur);
-
-    //                 while (len > 0) {
-    //                     arr.push(res.pop());
-    //                     len--;
-    //                 }
-
-    //                 console.log('arr', arr);
-    //             }
-    //         }
-    //     }
-    // });
-
-    return res.join('');
-}
-console.log('test --->>>', test('））（（）））（）（）（（'));
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/remove-invalid-parentheses/solution/shan-chu-wu-xiao-de-gua-hao-by-leetcode-9w8au/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+console.log('removeInvalidParentheses', removeInvalidParentheses('))(())))()()(('));
+console.log('removeInvalidParentheses', removeInvalidParentheses('))((())))()()(('));
