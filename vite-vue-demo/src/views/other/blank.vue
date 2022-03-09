eef57f1b7a83aa34689dccf82528bdfd05262bd5
lhd <1625125333@qq.com>
2022-03-09 13:57:54 +0800

    测试 computed 和 watch 的优先级

--- vite-vue-demo/src/views/other/blank.vue
+++ vite-vue-demo/src/views/other/blank.vue
@@ -2,12 +2,14 @@
     <div class="blank">
         <h3>blank</h3>
         <span v-bind:title="message"> 鼠标悬停几秒钟查看此处动态绑定的提示信息！ </span>
+        <div>{{ computedMsg }}</div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, getCurrentInstance } from 'vue';
 import vue from 'vue';
+import { log } from 'console';
 // import About from './About.vue';
 export default defineComponent({
     // components: {
@@ -15,8 +17,27 @@ export default defineComponent({
     // },
     data() {
         return {
+            msg1: 111,
+            msg2: 222,
             message: '页面加载于 ' + new Date().toLocaleString(),
         };
+    },
+    computed: {
+        computedMsg: function () {
+            console.log('cpmputed msg1 msg2: ', this.msg1, this.msg2);
+            return 'computed-' + this.msg1 + '-' + this.msg2;
+        },
+    },
+    watch: {
+        msg1: function () {
+            console.log('watch msg1: ', this.msg1);
+        },
+        msg2: function () {
+            console.log('watch msg2: ', this.msg2);
+        },
+        computedMsg: function () {
+            console.log('watch computedMsg: ', this.computedMsg);
+        },
     },
     setup() {
         // console.log('blank  getCurrentInstance---------------------------------->>>', getCurrentInstance());
@@ -28,6 +49,14 @@ export default defineComponent({
         // console.log('blank  vue---------------------------------->>>', vue);
         // console.log('blank  this---------------------------------->>>', this);
         // console.log('blank  this.r1 === this.r2---------------------------------->>>', this.r1 === this.r2);
+        this.msg1 = 1;
+        this.msg2 = 2;
+        // setTimeout(() => {
+        //     this.msg1 = 'updatedMsg1';
+        //     setTimeout(() => {
+        //         this.msg2 = 'updatedMsg2';
+        //     }, 0);
+        // }, 1000);
     },
 });
 </script>
